a4b7c60653ca64e52330d5f1e38f879bccd28253
CharlieS115 <cstorey115@gmail.com>
2021-07-16 11:57:40 +0000

    Added scripting for form drop downs and selects.

---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav({edge: 'right'});
+    $('.datepicker').datepicker({
+      format: 'dd.mm.yyyy',
+      yearRange: 1,
+      showClearBtn: true,
+      i18n: {
+        done: 'select'
+      }
+    });
+    $('.timepicker').timepicker({
+      defaultTime: '09:00',
+      twelveHour: false
+    });
+    $('select').formSelect();
+    
+  });
